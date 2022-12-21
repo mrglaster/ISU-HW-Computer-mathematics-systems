1bf01dce0ce58cf075e0f76c4343707f33ecf313
Egor Pristavka <pristavkaegor03@gmail.com>
2022-12-22 03:08:30 +0800

    Delete ModularProject/modules/interpolation_extrapolation directory

--- ModularProject/modules/interpolation_extrapolation/graphical_demonstration.py
+++
@@ -1,6 +0,0 @@
-from modules.matrix_operations import *
-from modules.vector_operations import *
-from modules.matrix_operations.matrix_operations import *
-from modules.matrix_operations.matrix_parameters import *
-from modules.interpolation_extrapolation.linear_inter_extrap import *
-
