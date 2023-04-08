2d472a02a773c9dd37de451f6ceea5e734819ef9
Egor Pristavka <pristavkaegor03@gmail.com>
2023-04-08 21:27:09 +0800

    Update main.py

--- ModularProject/main.py
+++ ModularProject/main.py
@@ -1,4 +1,8 @@
 from modules.linear_equations_system.les_solver import *
 
-test_matrix = [[1, 2], [3, 4]]
-print(matrix_inverce_test(test_matrix))
+def main():
+  test_matrix = [[1, 2], [3, 4]]
+  print(matrix_inverce_test(test_matrix))
+
+if __name__ == '__main__':
+  main()
